c4fe1c03886d6162ac2e5fb0b65b4f5e71f1823e
rauantasbolat <rauan98ktl@gmail.com>
2022-10-26 01:01:33 +0600

    d

---
+++ app/src/main/java/rauantasbolat/movies/MainActivity.kt
@@ -0,0 +1,11 @@
+package rauantasbolat.movies
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
